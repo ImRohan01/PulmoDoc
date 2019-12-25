ae217a96caead101c4b005bf6649565888c01037
Rohan Shetty <44741853+ImRohan01@users.noreply.github.com>
2019-12-26 01:22:03 +0530

    Delete doctor.sql

--- PulmoDoc/doctor.sql
+++
@@ -1,9 +0,0 @@
-USE `pulmodoc`;
-CREATE TABLE IF NOT EXISTS `patient_register` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-  	`fullname` varchar(100) NOT NULL,
-    `email` varchar(100) NOT NULL,
-  	`password` varchar(255) NOT NULL,
-    `phone` int(10) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
